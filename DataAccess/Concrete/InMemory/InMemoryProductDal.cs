930e78015e5025ac4ac038215e62de60d9ef2778
DuranUnverdi <75857104+DuranUnverdi@users.noreply.github.com>
2021-02-19 01:09:04 +0300

    firs commit

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product{CategoryId=1,ProductId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+                 new Product{CategoryId=1,ProductId=2,ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
+                  new Product{CategoryId=2,ProductId=3,ProductName="Buzdolabı",UnitPrice=3000,UnitsInStock=5},
+                   new Product{CategoryId=2,ProductId=4,ProductName="Televizyon",UnitPrice=15000,UnitsInStock=150},
+                    new Product{CategoryId=3,ProductId=5,ProductName="Klavye",UnitPrice=150,UnitsInStock=10},
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete =_products.SingleOrDefault(p=>p.ProductId==product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+          return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            Product productUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productUpdate.ProductName = product.ProductName;
+            productUpdate.CategoryId = product.CategoryId;
+            productUpdate.UnitPrice = product.UnitPrice;
+            productUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
