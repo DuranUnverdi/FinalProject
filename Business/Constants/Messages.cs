cb15089fe70f7f6a693031971e4e8f4dcfa82b19
DuranUnverdi <75857104+DuranUnverdi@users.noreply.github.com>
2021-03-24 19:58:24 +0300

    24 mart

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -9,7 +9,11 @@ namespace Business.Constants//sabitler
     {
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
-        internal static string MaintenanceTime="Sistem bakımda";
-        internal static string ProductsListed="Ürünler listelendi";
+        public static string MaintenanceTime="Sistem bakımda";
+        public static string ProductsListed="Ürünler listelendi";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 adaet ürün bulunabilir ";
+        public static string ProductNameAlreadyExists = "Aynı isimde ürün bulunmaktadır ";
+
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı için teni ürün eklenemiyor";
     }
 }
