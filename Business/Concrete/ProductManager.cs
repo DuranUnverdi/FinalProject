9632e01459608814347feb25ce4513e3c569549f
DuranUnverdi <75857104+DuranUnverdi@users.noreply.github.com>
2021-03-28 19:08:06 +0300

    28 mart commit 1

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -27,6 +27,7 @@ namespace Business.Concrete
             _productDal = productDal;
             _categoryService = categoryService;
         }
+        //[SecuredOperation("product.add")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
